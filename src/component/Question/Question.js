8f9b94e8b525ec951ff2c1df236c196a57cfc8b6
chibuezej <Johnhenrychibueze@gmail.com>
2023-02-20 19:49:07 +0100

    added question fixed quiz

--- src/component/Question/Question.js
+++ src/component/Question/Question.js
@@ -1,7 +1,56 @@
-function Question() {
+import { Button } from "@material-ui/core";
+import { useState } from "react";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import './Question.css'
+
+function Question({currQues, setCurrQues,questions, options, correct, score, setScore, setQuestions }) {
+
+
+    const [selected, setSelected] = useState()
+    const [error, setError] = useState(false)
+
+    function handleSelect(item){
+        if (selected === item && selected === correct){
+            return "select"
+        }
+        else if (selected === item && selected !== correct){
+            return "wrong"
+        }else if (item === correct){
+            return "select"
+        }
+    }
+    function handleCheck(item){
+        setSelected(item)
+        if(item === correct) setScore(score +1)
+        setError(false)
+    }
     return(
-        <div>
-            Question
+        <div className="question">
+            <h1>Question {currQues +1}</h1>
+            <div className="singleQuestion">
+                <h2>{questions[currQues].question}</h2>
+
+                <div className="options">
+                {error && <ErrorMessage>Please fill all the fields</ErrorMessage>}
+                    {options && options.map((item) => 
+                    <button onClick={handleCheck} className={`singleOption ${selected && handleSelect(item)}`} key={item} disabled={selected}>{item}</button>)}
+                
+                </div>
+                <div className="controls">
+                    <Button
+                    variant="contained"
+                    color="secondary"
+                    size="large"
+                    style={{width: 185}}
+                    href="/"
+                    >Quit</Button>
+                    <Button
+                     variant="contained"
+                     color="primary"
+                     size="large"
+                    >Next Question</Button>
+                </div>
+            </div>
         </div>
     )
 }
--- src/component/pages/Quiz/Quiz.js
+++ src/component/pages/Quiz/Quiz.js
@@ -1,50 +1,66 @@
 
+import { CircularProgress } from "@material-ui/core";
 import { useEffect, useState } from "react";
+import Question from "../../Question/Question";
 import './Quiz.css'
 
 function Quiz ({name, score, questions, setQuestions, setScore}){
-    const [options, setOptions] = useState([]);
+    const [options, setOptions] = useState();
     const [currQues, setCurrQues] = useState(0)
     //currQues is to be set to zero 
     
+    useEffect(() => {
+      setOptions(
+        questions &&
+          handleShuffle([
+            questions[currQues]?.correct_answer,
+            ...questions[currQues]?.incorrect_answers,
+          ])
+      );
+    }, [currQues, questions]);
 
-    
-
-    // useEffect(() => {
-
-    //   // setOptions(questions && 
-    //   //   handleShuffle([...questions]))
-    //   // handleShuffle([questions[currQues]?.correct_answer,
-    //   //   ...questions[currQues]?.incorect_answers]))
-    
-      
-    //    //not getting my options  
-    //     setOptions(questions && 
-    //       handleShuffle([questions[currQues]?.correct_answer,
-    //         ...questions[currQues]?.incorect_answers]))
-        
-    // }, [questions])
-// console.log(options, "options")
-console.log(questions, "questions")
-//i.e when i console log my options 
-    
+    function handleShuffle (options) {
+      return options.sort(() => Math.random() - 0.5);
+    };
 
 
-    function handleShuffle(optionss){
-      console.log(questions.sort(() => Math.random() - 0.5))
-
-        // console.log(optionss.sort(() => Math.random() - 0.5), "random")
-    }
-
-
-// handleShuffle([...questions])
 
     return(
-        <div>
+        <div className="quiz">
             
             <span className="subtitle">Welcome, {name}</span>
 
-             <div> {questions && questions?.map((item) => (
+            {questions ? (
+        <>
+          <div className="quizInfo">
+            <span>{questions[currQues].category}</span>
+            <span>
+              {/* {questions[currQues].difficulty} */}
+              Score : {score}
+              
+            </span>
+          </div>
+          <Question
+            currQues={currQues}
+            setCurrQues={setCurrQues}
+            questions={questions}
+            options={options}
+            correct={questions[currQues]?.correct_answer}
+            score={score}
+            setScore={setScore}
+            setQuestions={setQuestions}
+          />
+        </>
+      ) : (
+        <CircularProgress
+          style={{ margin: 100 }}
+          color="inherit"
+          size={150}
+          thickness={1}
+        />
+      )}
+
+             {/* <div> {questions && questions?.map((item) => (
               // <h1> {item.catego ry}</h1>
               <div  style={{paddingLeft: "20px"}} >
                 <ul>
@@ -60,7 +76,7 @@ console.log(questions, "questions")
                 </ul>
               <h6>Answers: {item.correct_answer}</h6>
 </div>
-            ) )}</div>
+            ) )}</div> */}
            
         </div>
     )
